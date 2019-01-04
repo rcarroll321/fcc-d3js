f840b1f1cdee91686964ed5d9e98731849cbdcdf
adeleon <adeleon@linode.com>
2019-01-04 09:53:04 -0500

    fix show both on one page

--- chart1.js
+++ chart1.js
@@ -2,57 +2,57 @@
 
 // Data for Chart One: Interest level in the course
 const chart1 = [
-   {
+  {
     interest: 'Low',
     value: 1,
-    color: '#00a2ee'
-   },
-   {
+    color: '#00a2ee',
+  },
+  {
     interest: 'Moderate',
     value: 5,
-    color: '#fbcb39'
-   },
-   {
+    color: '#fbcb39',
+  },
+  {
     interest: 'High',
     value: 4,
-    color: '#65cedb'
-   },
-
+    color: '#65cedb',
+  },
 ]
 
 // Code for Chart One
-const svg = d3.select('svg');
-const svgContainer = d3.select('#chart-one');
+const svg = d3.select('.svg1')
 
-const margin = 80;
-const width = 1000 - 2 * margin;
-const height = 600 - 2 * margin;
+const margin = 80
+const width = 1000 - 2 * margin
+const height = 600 - 2 * margin
 
-const chart = svg.append('g')
-  .attr('transform', `translate(${margin}, ${margin})`);
+const chart = svg
+  .append('g')
+  .attr('transform', `translate(${margin}, ${margin})`)
 
-const xScale = d3.scaleBand()
+const xScale = d3
+  .scaleBand()
   .range([0, width])
-  .domain(chart1.map((s) => s.interest))
+  .domain(chart1.map(s => s.interest))
   .padding(0.4)
 
-const yScale = d3.scaleLinear()
+const yScale = d3
+  .scaleLinear()
   .range([height, 0])
-  .domain([0, 10]);
+  .domain([0, 10])
 
 // vertical grid lines
 // const makeXLines = () => d3.axisBottom()
 //   .scale(xScale)
 
-const makeYLines = () => d3.axisLeft()
-  .scale(yScale)
+const makeYLines = () => d3.axisLeft().scale(yScale)
 
-chart.append('g')
+chart
+  .append('g')
   .attr('transform', `translate(0, ${height})`)
-  .call(d3.axisBottom(xScale));
+  .call(d3.axisBottom(xScale))
 
-chart.append('g')
-  .call(d3.axisLeft(yScale));
+chart.append('g').call(d3.axisLeft(yScale))
 
 // vertical grid lines
 // chart.append('g')
@@ -63,14 +63,17 @@ chart.append('g')
 //     .tickFormat('')
 //   )
 
-chart.append('g')
+chart
+  .append('g')
   .attr('class', 'grid')
-  .call(makeYLines()
-    .tickSize(-width, 0, 0)
-    .tickFormat('')
+  .call(
+    makeYLines()
+      .tickSize(-width, 0, 0)
+      .tickFormat('')
   )
 
-const barGroups = chart.selectAll()
+const barGroups = chart
+  .selectAll()
   .data(chart1)
   .enter()
   .append('g')
@@ -78,69 +81,68 @@ const barGroups = chart.selectAll()
 barGroups
   .append('rect')
   .attr('class', 'bar')
-  .attr('x', (g) => xScale(g.interest))
-  .attr('y', (g) => yScale(g.value))
-  .attr('height', (g) => height - yScale(g.value))
+  .attr('x', g => xScale(g.interest))
+  .attr('y', g => yScale(g.value))
+  .attr('height', g => height - yScale(g.value))
   .attr('width', xScale.bandwidth())
-  .on('mouseenter', function (actual, i) {
-    d3.selectAll('.value')
-      .attr('opacity', 0)
+  .on('mouseenter', function(actual, i) {
+    d3.selectAll('.value').attr('opacity', 0)
 
     d3.select(this)
       .transition()
       .duration(300)
       .attr('opacity', 0.6)
-      .attr('x', (a) => xScale(a.interest) - 5)
+      .attr('x', a => xScale(a.interest) - 5)
       .attr('width', xScale.bandwidth() + 10)
 
     const y = yScale(actual.value)
 
-    line = chart.append('line')
+    line = chart
+      .append('line')
       .attr('id', 'limit')
       .attr('x1', 0)
       .attr('y1', y)
       .attr('x2', width)
       .attr('y2', y)
 
-    barGroups.append('text')
+    barGroups
+      .append('text')
       .attr('class', 'divergence')
-      .attr('x', (a) => xScale(a.interest) + xScale.bandwidth() / 2)
-      .attr('y', (a) => yScale(a.value) + 30)
+      .attr('x', a => xScale(a.interest) + xScale.bandwidth() / 2)
+      .attr('y', a => yScale(a.value) + 30)
       .attr('fill', 'white')
       .attr('text-anchor', 'middle')
       .text((a, idx) => {
-        const divergence = (a.value - actual.value)
-        
+        const divergence = a.value - actual.value
+
         let text = ''
         if (divergence > 0) text += '+'
         text += `${divergence}`
 
-        return idx !== i ? text : '';
+        return idx !== i ? text : ''
       })
-
   })
-  .on('mouseleave', function () {
-    d3.selectAll('.value')
-      .attr('opacity', 1)
+  .on('mouseleave', function() {
+    d3.selectAll('.value').attr('opacity', 1)
 
     d3.select(this)
       .transition()
       .duration(300)
       .attr('opacity', 1)
-      .attr('x', (a) => xScale(a.interest))
+      .attr('x', a => xScale(a.interest))
       .attr('width', xScale.bandwidth())
 
     chart.selectAll('#limit').remove()
     chart.selectAll('.divergence').remove()
   })
 
-barGroups 
+barGroups
   .append('text')
   .attr('class', 'value')
-  .attr('x', (a) => xScale(a.interest) + xScale.bandwidth() / 2)
-  .attr('y', (a) => yScale(a.value) + 30)
+  .attr('x', a => xScale(a.interest) + xScale.bandwidth() / 2)
+  .attr('y', a => yScale(a.value) + 30)
   .attr('text-anchor', 'middle')
-  .text((a) => `${a.value}`)
+  .text(a => `${a.value}`)
 
 svg
   .append('text')
@@ -151,24 +153,28 @@ svg
   .attr('text-anchor', 'middle')
   .text('# of Respondents')
 
-svg.append('text')
+svg
+  .append('text')
   .attr('class', 'label')
   .attr('x', width / 2 + margin)
   .attr('y', height + margin * 1.7)
   .attr('text-anchor', 'middle')
   .text('Interest Level')
 
-svg.append('text')
+svg
+  .append('text')
   .attr('class', 'title')
   .attr('x', width / 2 + margin)
   .attr('y', 40)
   .attr('text-anchor', 'middle')
-  .text('Before enrolling, my left of interest in the subject matter of this course was...')
+  .text(
+    'Before enrolling, my left of interest in the subject matter of this course was...'
+  )
 
-svg.append('text')
+svg
+  .append('text')
   .attr('class', 'source')
   .attr('x', width - margin / 2)
   .attr('y', height + margin * 1.7)
   .attr('text-anchor', 'start')
   .text('Source: Krusty Klown College, 2018')
-
